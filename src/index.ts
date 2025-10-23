3b997e993ef8f9568ef3cbfd11aefdc90601b407
ANovokmet <ante.je.zakon@gmail.com>
2025-10-23 22:28:47 +0200

    add vite plugin

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
 import transformer from './transform';
 import transpiler from './transpile';
 
-function parse(source) {
+export function parse(source: string) {
     const ast = transformer.parse(source);
     const target = transpiler.parse(ast);
     return target;
 }
 
-const transform = (src) => transformer.parse(src);
-const transpile = (ast) => transpiler.parse(ast);
-
-export {
-    transform,
-    transpile,
-    parse
-}
+export const transform = (src) => transformer.parse(src);
+export const transpile = (ast) => transpiler.parse(ast);
